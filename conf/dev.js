227ed7678cb67bd3b3672fb44f611577b70f8f41
neveer <zengchong.dd@gmail.com>
2017-07-04 12:42:12 +0800

    Update dev.js

--- conf/dev.js
+++ conf/dev.js
@@ -4,6 +4,11 @@ var baseConfig = require('./base.js');
 var config =Object.assign({},baseConfig,{
   devtool:'cheap-module-source-map'
 });
+config.plugins.push({
+  'process.env.MODE_ENV':'"development"'
+});
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
+config.plugins.push(new webpack.NamedModulePlugin());
 
 config.devServer = {
   port: 7777,
