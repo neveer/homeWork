7dff5214c84ebfaf2e96ac97ccf939a92d1be41b
neveer <zengchong.dd@gmail.com>
2017-07-04 10:55:53 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
-module.exports=function(env){
-    return require(`./conf/${env}.js`);
+
+var args=require('minimist')(process.argv.slice(2));
+var env = 'dev';
+var allowedEnv =['dev','prod'];
+if(args.env){
+    env=args.env;
 }
+function buildConfig(env){
+    var isValid = env && env.length > 0 && allowedEnv.contains(env);
+    var validEnv = isValid ?env:'dev';
+    return require(`./conf/${validEnv}.js`);
+}
+module.exports=buildConfig(env);
 
